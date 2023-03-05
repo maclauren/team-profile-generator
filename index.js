d0fad817c23fda34cbc7a05da8aa0a2b5a6f9733
lauren-mac <109279133+lauren-mac@users.noreply.github.com>
2023-03-05 20:12:24 +0000

    manager prompts and create new manager

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// Constructors
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Dependencies
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+// Paths
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +19,60 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Manager prompts
+function addManager() {
+    console.log("Build your team profile");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "Enter the managers name",
+            validate: answer =>  {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Name cannot be left empty";
+            }
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "Enter the managers ID",
+            validate: answer =>  {
+                if (answer !== "") {
+                    return true;
+                }
+                return "ID cannot be left empty";
+            }
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "Enter the managers email",
+            validate: answer =>  {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Email cannot be left empty";
+            }
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "Enter the managers office number",
+            validate: answer =>  {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Office number cannot be left empty";
+                }
+            }
+    ])
+    
+    .then(answers =>{
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamInput.push(manager);
+        idInput.push(answers.managerId);
+        addTeam();
+    });
+}
